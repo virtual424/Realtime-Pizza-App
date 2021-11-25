64170350cd6f3b370a4fdfb3d6da2ae577e6d05b
virtual424 <virtualettin586@gmail.com>
2021-11-25 18:49:06 +0530

    firebase file edited

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,1 @@
-import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyADcxCDHIGzQTs5Ujd0IRUyiPdkM3CzvlA",
-  authDomain: "realtime-pizza-app-3c73c.firebaseapp.com",
-  projectId: "realtime-pizza-app-3c73c",
-  storageBucket: "realtime-pizza-app-3c73c.appspot.com",
-  messagingSenderId: "405690468892",
-  appId: "1:405690468892:web:fbdb8e044bd5218e066dd9",
-};
-
-const app = initializeApp(firebaseConfig);
